2adcad2857e0b265cb08d7683c9757a29f11261a
Key5n <k.nogami.472@stn.nitech.ac.jp>
2023-04-26 04:52:11 +0900

    update: introduce trpc

---
+++ src/server/routers/_app.ts
@@ -0,0 +1,55 @@
+import { z } from 'zod';
+import { procedure, router } from '../trpc';
+import { dataSchema } from '@/types';
+import { Client } from '@notionhq/client';
+
+export const appRouter = router({
+  pages: procedure.input(dataSchema).mutation(async ({ input }) => {
+    const notion = new Client({
+      auth: process.env.NOTION_TOKEN,
+    });
+    input.forEach((item) => {
+      notion.pages.update({
+        page_id: item.page_id,
+        properties: {
+          Checkbox: item.checkbox,
+        },
+      });
+    });
+
+    const databaseId = process.env.NOTION_DATABASE_ID;
+    if (typeof databaseId === 'undefined') {
+      throw new Error('Notion database id not defined');
+    }
+    const myPage = await notion.databases.query({
+      database_id: databaseId,
+    });
+    const values = myPage.results
+      .map((item) => {
+        let isChecked;
+        let name;
+        if ('properties' in item) {
+          if (item.properties.Checkbox.type === 'checkbox') {
+            isChecked = item.properties.Checkbox.checkbox;
+          }
+          if (item.properties.Name.type === 'title') {
+            name = item.properties.Name.title[0].plain_text;
+          }
+        } else {
+          throw new Error('properties not found in results');
+        }
+        return {
+          page_id: item.id,
+          checkbox: isChecked,
+          name: name,
+        };
+      })
+      .reverse();
+    return {
+      input,
+    };
+  }),
+});
+
+// export type definition of API
+export type AppRouter = typeof appRouter;
